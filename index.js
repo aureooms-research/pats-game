d12607b4f8cf3566f6ec5c6ef62963239496596d
aureooms <aurelien.ooms@gmail.com>
2016-03-18 20:15:51 +0100

    lots of progress

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
 'use strict' ;
 
+function logGitHubAPIRequest ( event ) {
+
+	var log = document.getElementById('log') ;
+	log.classList.remove('status-pending');
+
+	var status = event.target.status ;
+	var statusText = event.target.statusText ;
+	var reset = event.target.getResponseHeader('X-RateLimit-Reset') ;
+	var limit = event.target.getResponseHeader('X-RateLimit-Limit') ;
+	var remaining = event.target.getResponseHeader('X-RateLimit-Remaining') ;
+	reset = new Date( parseInt(reset) * 1000 ).toLocaleFormat() ;
+	limit = parseInt( limit ) ;
+	remaining = parseInt( remaining ) ;
+
+	document.getElementById('log-status').innerText = status ;
+	document.getElementById('log-status-text').innerText = statusText ;
+	document.getElementById('log-reset').innerText = reset ;
+	document.getElementById('log-limit').innerText = limit;
+	document.getElementById('log-remaining').innerText = remaining ;
+
+	if ( status !== 200 ) {
+		log.classList.add('status-error');
+		console.error('There was a problem with the request.', event);
+		console.error( status , statusText ) ;
+		try{
+			var message = JSON.parse(event.target.responseText).message ;
+			document.getElementById('log-message').innerText = message ;
+			console.error( 'response is', message);
+		}
+		catch (e){
+			console.error( 'response is not in JSON format');
+			console.error( 'responce is', event.target.responseText);
+		}
+	}
+	else {
+		log.classList.add('status-ok');
+	}
+}
+
+function logGitHubRAWRequest ( event ) {
+	var status = event.target.status ;
+	var statusText = event.target.statusText ;
+
+	if ( status !== 200 ) {
+		console.error('There was a problem with the request.', event);
+		console.error( status , statusText ) ;
+		console.error( 'response is', event.target.responseText);
+	}
+	else {
+		console.debug( status , statusText ) ;
+	}
+}
+
 function Color ( red , green , blue ) {
 	this.red = red ;
 	this.green = green ;
@@ -67,21 +120,27 @@ var OWNER = 'aureooms-research' ;
 var REPO = 'pats-game-solutions' ;
 var URL_TREE = GITHUB_API + '/repos/' + OWNER + '/' + REPO + '/git/trees/master?recursive=true' ;
 var ROOT = 'https://github.com/aureooms-research/pats-game-solutions/blob/master/' ;
+var RAW = 'https://raw.githubusercontent.com/aureooms-research/pats-game-solutions/master/' ;
 
-function Solution ( M , N , R , score , hash , metadata ) {
+function Solution ( M , N , R , score , sat , hash , metadata ) {
 	this.M = M ;
 	this.N = N ;
 	this.R = R ;
 	this.score = score ;
+	this.sat = sat ;
 	this.hash = hash ;
 	this.metadata = metadata ;
 	this.moves = null ;
 	this.html = this.inithtml();
-	this.lazyload();
+	if ( this.sat ) this.lazyload();
 }
 
 Solution.prototype.dimensions = function ( ) {
 	return this.M + 'x' + this.N + 'x' + this.R ;
+} ;
+
+Solution.prototype.url = function ( ) {
+	return RAW + this.dimensions() + '/' + ( this.sat ? 'sat' : 'unsat' ) + '/'+ this.score + '/' + this.hash ;
 } ;
 
 Solution.prototype.grid = function ( ) {
@@ -130,8 +189,9 @@ Solution.prototype._html_head = function ( ) {
 		tags : {
 			root : document.createElement('div') ,
 			score :{ root: document.createElement('span') } ,
-			bestknown :{ root: document.createElement('span') } ,
-			optimal :{ root: document.createElement('span') }
+			best :{ root: document.createElement('span') } ,
+			optimal :{ root: document.createElement('span') } ,
+			unsat :{ root: document.createElement('span') } ,
 		} ,
 		hash :{ root: document.createElement('a') }
 	};
@@ -155,12 +215,15 @@ Solution.prototype._html_head = function ( ) {
 	head.tags.score.root.innerText = this.score ;
 	head.tags.score.root.classList.add('tag');
 	head.tags.score.root.classList.add('tag-score');
-	head.tags.bestknown.root.innerText = 'best' ;
-	head.tags.bestknown.root.classList.add('tag');
-	head.tags.bestknown.root.classList.add('tag-best');
+	head.tags.best.root.innerText = 'best' ;
+	head.tags.best.root.classList.add('tag');
+	head.tags.best.root.classList.add('tag-best');
 	head.tags.optimal.root.innerText = 'opt' ;
 	head.tags.optimal.root.classList.add('tag');
 	head.tags.optimal.root.classList.add('tag-opt');
+	head.tags.unsat.root.innerText = 'unsat' ;
+	head.tags.unsat.root.classList.add('tag');
+	head.tags.unsat.root.classList.add('tag-unsat');
 
 	head.hash.root.innerText = this.hash ;
 	head.hash.root.href = ROOT + this.metadata.path ;
@@ -172,8 +235,9 @@ Solution.prototype._html_head = function ( ) {
 	head.dimensions.root.appendChild(head.dimensions.rounds.root);
 
 	head.tags.root.appendChild(head.tags.score.root);
-	head.tags.root.appendChild(head.tags.bestknown.root);
+	head.tags.root.appendChild(head.tags.best.root);
 	head.tags.root.appendChild(head.tags.optimal.root);
+	head.tags.root.appendChild(head.tags.unsat.root);
 
 	head.root.appendChild(head.dimensions.root);
 	head.root.appendChild(head.tags.root);
@@ -215,6 +279,19 @@ Solution.prototype._html_grid_preview = function ( ) {
 	} ;
 
 	grid.root.appendChild(grid.tbody.root);
+
+	if ( !this.sat ) {
+
+		var row = document.createElement('tr');
+		grid.tbody.root.appendChild(row);
+
+		var cell = document.createElement('td');
+		row.appendChild(cell);
+
+		cell.innerText = 'unsatisfiable';
+		return grid ;
+
+	}
 
 	var M = this.M ;
 	var N = this.N ;
@@ -279,7 +356,12 @@ Solution.parse_moves = function ( raw ) {
 	var N = parseInt(header[1]) ;
 	var R = parseInt(header[2]) ;
 	var score = parseInt(lines[1]) ;
-	var _moves = lines.slice(2);
+	var sat = !!parseInt(lines[2]) ;
+	if ( !sat ) {
+		console.error('cannot parse moves if unsatisfiable') ;
+		return null ;
+	}
+	var _moves = lines.slice(3);
 	var moves = [ ] ;
 	for ( var m = 0 ; m < score ; ++m ) {
 		var _move = _moves[m].split(' ');
@@ -297,17 +379,18 @@ Solution.parse_moves = function ( raw ) {
 Solution.from = function ( metadata ) {
 
 	var parts = metadata.path.split('/') ;
-	if ( parts.length < 3 ) return null ;
+	if ( parts.length < 4 ) return null ;
 
 	// this is a path to a solution
 	var dimensions = parts[0].split('x') ;
 	var rows = parseInt(dimensions[0]);
 	var columns = parseInt(dimensions[1]);
 	var rounds = parseInt(dimensions[2]);
-	var score = parseInt(parts[1]);
-	var hash = parts[2];
+	var sat = parts[1] === 'sat';
+	var score = parseInt(parts[2]);
+	var hash = parts[3];
 
-	return new Solution( rows , columns , rounds , score , hash , metadata ) ;
+	return new Solution( rows , columns , rounds , score , sat , hash , metadata ) ;
 
 } ;
 
@@ -317,32 +400,21 @@ Solution.prototype.lazyload = function ( ) {
 
 	var onreadystatechange = function ( event ) {
 		 if (event.target.readyState === XMLHttpRequest.DONE) {
+			logGitHubRAWRequest( event ) ;
 			if (event.target.status === 200) {
-				var response = event.target.responseText ;
-				var object = JSON.parse(response);
-				if (object.encoding !== 'base64'){
-					console.error('wrong encoding');
-					console.debug('event',event);
-					console.debug('object',object);
-					return ;
-				}
-				var len = object.content.length ;
-				var raw = atob(object.content.substr(0,len-1));
+				var raw = event.target.responseText ;
 				var moves = Solution.parse_moves( raw ) ;
 				solution.moves = moves ;
 				var grid = solution._html_grid( ) ;
 				solution.html.body.root.replaceChild(grid.root, solution.html.body.grid.root);
 				solution.html.body.grid = grid ;
-			} else {
-				console.error('There was a problem with the request.');
-				console.debug( 'event', event);
 			}
 		}
 	} ;
 
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = onreadystatechange ;
-	var url = solution.metadata.url ;
+	var url = solution.url() ;
 	request.open('GET', url);
 	request.send();
 
@@ -350,17 +422,22 @@ Solution.prototype.lazyload = function ( ) {
 
 
 var Loader = function ( ) {
-	this.solutions = [ ] ;
+	this.solutions = { root : document.createElement( 'div' ) , list : [ ] } ;
+	this.lb = { } ;
+	this.ub = { } ;
+
+	this.solutions.root.classList.add( 'solutions' );
+	document.getElementById('solutions-container').appendChild(this.solutions.root);
 } ;
 
 Loader.prototype.update_exponent = function ( ){
 	var best = 3/2 ;
 	var html = '3/2 = 1.5' ;
-	var n = this.solutions.length ;
+	var n = this.solutions.list.length ;
 	for (var k = 0 ; k < n ; ++k ) {
-		var solution = this.solutions[k];
+		var solution = this.solutions.list[k];
 		var d = solution.M ;
-		if ( solution.N === d && solution.R === d ) {
+		if ( d > 1 && solution.sat && solution.N === d && solution.R === d ) {
 			var s = solution.score;
 			var a = Math.log(s) / Math.log(d) ;
 			if ( a >= best ) {
@@ -372,28 +449,62 @@ Loader.prototype.update_exponent = function ( ){
 	document.getElementById('exponent').innerHTML = html ;
 } ;
 
-Loader.prototype.update_best = function ( ) {
-	var solution, k;
-	var best = { } ;
-	var n = this.solutions.length ;
+Loader.prototype.update_ub = function ( ) {
+	var ub = this.ub ;
+	var n = this.solutions.list.length ;
 
-	for (k = 0 ; k < n ; ++k ) {
+	for (var k = 0 ; k < n ; ++k ) {
 
-		solution = this.solutions[k];
+		var solution = this.solutions.list[k];
+
+		if ( solution.sat ) continue ;
 
 		var id = solution.dimensions() ;
 
-		if ( best[id] === undefined || solution.score > best[id] ) {
-			best[id] = solution.score ;
+		if ( ub[id] === undefined || solution.score <= ub[id] ) {
+			ub[id] = solution.score - 1 ;
 		}
 
 	}
 
-	for (k = 0 ; k < n ; ++k ) {
+} ;
 
-		solution = this.solutions[k];
+Loader.prototype.update_lb = function ( ) {
+	var lb = this.lb ;
+	var n = this.solutions.list.length ;
 
-		if ( solution.score === best[solution.dimensions()] ) {
+	for (var k = 0 ; k < n ; ++k ) {
+
+		var solution = this.solutions.list[k];
+
+		if ( !solution.sat ) continue ;
+
+		var id = solution.dimensions() ;
+
+		if ( lb[id] === undefined || solution.score > lb[id] ) {
+			lb[id] = solution.score ;
+		}
+
+	}
+
+} ;
+
+Loader.prototype.update_tags = function ( ) {
+
+	var n = this.solutions.list.length ;
+	for (var k = 0 ; k < n ; ++k ) {
+
+		var solution = this.solutions.list[k];
+
+		if ( !solution.sat ) {
+			solution.html.root.classList.add( 'unsat' ) ;
+		}
+
+		else if ( solution.score === this.ub[solution.dimensions()] ) {
+			solution.html.root.classList.add( 'opt' ) ;
+		}
+
+		else if ( solution.score === this.lb[solution.dimensions()] ) {
 			solution.html.root.classList.add( 'best' ) ;
 		}
 
@@ -401,14 +512,35 @@ Loader.prototype.update_best = function ( ) {
 
 } ;
 
+Loader.prototype.update_dom = function ( ) {
+	var solutions = document.createElement('div');
+	solutions.classList.add( 'solutions' );
+
+	var n = this.solutions.list.length ;
+	for (var k = 0 ; k < n ; ++k ) {
+		var solution = this.solutions.list[k];
+		solutions.appendChild( solution.html.root ) ;
+	}
+
+	document.getElementById('solutions-container').replaceChild(solutions,this.solutions.root);
+	this.solutions.root = solutions;
+
+} ;
+
 Loader.prototype.update = function ( ) {
 
+	var log = document.getElementById('log') ;
+	log.classList.remove('status-error');
+	log.classList.remove('status-ok');
+	log.classList.add('status-pending');
+
 	var loader = this ;
-	loader.solutions.splice(0) ;
+	loader.solutions.list.splice(0) ;
 
 	var onreadystatechange = function ( event ) {
 		var solution;
 		if (event.target.readyState === XMLHttpRequest.DONE) {
+			logGitHubAPIRequest( event ) ;
 			if (event.target.status === 200) {
 				var raw = event.target.responseText ;
 				var object = JSON.parse(raw);
@@ -424,18 +556,17 @@ Loader.prototype.update = function ( ) {
 					console.debug( 'loading' , i + 1 , '/' , len ) ;
 					solution = Solution.from( tree[i] ) ;
 					if ( solution !== null ) {
-						loader.solutions.push(solution) ;
-						document.getElementById('solutions').appendChild(solution.html.root);
+						loader.solutions.list.push(solution) ;
 					}
 				}
 
-				console.debug( loader.solutions.length , 'solutions loaded');
+				console.debug( loader.solutions.list.length , 'solutions loaded');
 				loader.update_exponent( );
-				loader.update_best( );
+				loader.update_lb( );
+				loader.update_ub( );
+				loader.update_tags( );
+				loader.update_dom( );
 
-			} else {
-				console.error('There was a problem with the request.');
-				console.debug('event' , event);
 			}
 		}
 
@@ -448,5 +579,7 @@ Loader.prototype.update = function ( ) {
 
 } ;
 
-var loader = new Loader();
-loader.update();
+window.addEventListener( 'load' , function ( ) {
+	var loader = new Loader();
+	loader.update();
+} ) ;
